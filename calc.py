3122a96c1eeff3710b09ec13bc9540e4755f18d5
nrush <nrush4285@gmail.com>
2019-07-18 10:56:57 -0400

    merge fix

--- calc.py
+++ calc.py
@@ -1,5 +1,4 @@
 def compute(string):
-<<<<<<< HEAD
     """Perform simple arithmetic based on string input.
     
     Example: '5 + 7' -> 12
@@ -13,18 +12,6 @@ def compute(string):
     elif operator == '-':
     	return num0 - num1
     else:
-    	raise ValueError('Unknown operator!')
-=======
-	values = string.split(' ')
-	num0 = int(values[0])
-  	num1 = int(values[2])
-	operator = values[1]
-	if operator == '+':
-		return num0 + num1
-	elif operator == '-':
-		return num0 - num1
-	else:
-		msg = f'Unknown operator: "{operator}"'
-		msg += 'Choose from "+" and "-"'
-		raise ValueError(msg)
->>>>>>> better-error
+	msg = f'Unknown operator: "{operator}"'
+	msg += 'Choose from "+" and "-"'
+	raise ValueError(msg)

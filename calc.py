2b457c90c5919c9f61512d106bfdb63a07a9a05b
nrush <nrush4285@gmail.com>
2019-07-18 10:51:12 -0400

    another update

--- calc.py
+++ calc.py
@@ -1,12 +1,13 @@
 def compute(string):
-    values = string.split(' ')
-    num0 = int(values[0])
-    num1 = int(values[2]
-    operator = values[1]
-    if operator == '+':
-        return num0 + num1
-    elif operator == '-'
-    	return num0 - num1
-    else:
-    	msg = f'Unknown operator: "{operator}"'
-    	raise ValueError(msg)
+	values = string.split(' ')
+	num0 = int(values[0])
+  	num1 = int(values[2])
+	operator = values[1]
+	if operator == '+':
+		return num0 + num1
+	elif operator == '-':
+		return num0 - num1
+	else:
+		msg = f'Unknown operator: "{operator}"'
+		msg += 'Choose from "+" and "-"'
+		raise ValueError(msg)
